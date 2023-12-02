b5da04c22e0453c0fca9df089ab286b288944042
42-mki <mki@student.42seoul.kr>
2023-12-02 11:04:24 +0900

    Add comment in ex00

--- ex00/Hello.py
+++ ex00/Hello.py
@@ -3,9 +3,17 @@ ft_tuple = ("Hello", "toto!")
 ft_set = {"Hello", "tutu!"}
 ft_dict = {"Hello": "titi!"}
 
+# list는 mutable iterable이라 값을 바꿀 수 있습니다.
 ft_list[1] = "World!"
+
+# tuple은 immutable iterable이라 값을 바꿀 수 없습니다.
 ft_tuple = ("Hello", "Korea!")
-ft_set = {"Hello", "Seoul!"}
+
+# set은 Set Types입니다. 중복된 값을 허용하지 않습니다.
+ft_set.remove("tutu!")
+ft_set.add("Seoul!")
+
+# dict는 Mapping Types이며 key는 hashable object이고 value는 arbitrary object입니다.
 ft_dict["Hello"] = "42Seoul!"
 
 print(ft_list)
