02c82b477b654de4ccb6fcd53b975027ea1ff430
42-mki <mki@student.42seoul.kr>
2023-12-02 12:34:39 +0900

    Chore flake8

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -7,12 +7,11 @@ import datetime
 seconds_since_epoch = time.time()
 
 # https://docs.python.org/3/library/string.html#format-string-syntax
-# format_spec     ::=  [[fill]align][sign]["z"]["#"]["0"][width][grouping_option]["." precision][type]
 # grouping_option ::=  "_" | ","
-# type            ::=  "b" | "c" | "d" | "e" | "E" | "f" | "F" | "g" | "G" | "n" | "o" | "s" | "x" | "X" | "%"
 # type - f: floating point number
 # type - e: floating point number in exponential notation
-result = f"Seconds since January 1, 1970: {seconds_since_epoch:,.4f} or {seconds_since_epoch:.2e} in scientific notation"
+result = f"Seconds since January 1, 1970: {seconds_since_epoch:,.4f} or \
+{seconds_since_epoch:.2e} in scientific notation"
 print(result)
 
 # https://docs.python.org/3/library/datetime.html
