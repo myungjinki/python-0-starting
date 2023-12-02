d0470b68094819056dee382cafc6a685d41b2310
42-mki <mki@student.42seoul.kr>
2023-12-02 17:34:06 +0900

    Add ex07

---
+++ ex07/sos.py
@@ -0,0 +1,108 @@
+# https://en.wikipedia.org/wiki/Morse_code
+NESTED_MORSE = {
+    " ": "/ ",
+    "A": ".- ",
+    "B": "-... ",
+    "C": "-.-. ",
+    "D": "-.. ",
+    "E": ". ",
+    "F": "..-. ",
+    "G": "--. ",
+    "H": ".... ",
+    "I": ".. ",
+    "J": ".--- ",
+    "K": "-.- ",
+    "L": ".-.. ",
+    "M": "-- ",
+    "N": "-. ",
+    "O": "--- ",
+    "P": ".--. ",
+    "Q": "--.- ",
+    "R": ".-. ",
+    "S": "... ",
+    "T": "- ",
+    "U": "..- ",
+    "V": "...- ",
+    "W": ".-- ",
+    "X": "-..- ",
+    "Y": "-.-- ",
+    "Z": "--.. ",
+    "a": ".- ",
+    "b": "-... ",
+    "c": "-.-. ",
+    "d": "-.. ",
+    "e": ". ",
+    "f": "..-. ",
+    "g": "--. ",
+    "h": ".... ",
+    "i": ".. ",
+    "j": ".--- ",
+    "k": "-.- ",
+    "l": ".-.. ",
+    "m": "-- ",
+    "n": "-. ",
+    "o": "--- ",
+    "p": ".--. ",
+    "q": "--.- ",
+    "r": ".-. ",
+    "s": "... ",
+    "t": "- ",
+    "u": "..- ",
+    "v": "...- ",
+    "w": ".-- ",
+    "x": "-..- ",
+    "y": "-.-- ",
+    "z": "--.. ",
+    "0": "----- ",
+    "1": ".---- ",
+    "2": "..--- ",
+    "3": "...-- ",
+    "4": "....- ",
+    "5": "..... ",
+    "6": "-.... ",
+    "7": "--... ",
+    "8": "---.. ",
+    "9": "----. ",
+}
+
+
+def sos(morse_code):
+    """Convert morse code to text."""
+    return "".join([NESTED_MORSE[char] for char in morse_code])
+
+
+def ismorse(morse_code):
+    """Check if the morse code is valid."""
+    for char in morse_code:
+        if char not in NESTED_MORSE.keys():
+            return False
+    return True
+
+
+def main():
+    """Main function."""
+    import sys
+
+    if len(sys.argv) != 2:
+        print("AssertionError: the arguments are bad")
+        sys.exit(1)
+    morse_code = sys.argv[1]
+    if not ismorse(morse_code):
+        print(morse_code)
+        print("AssertionError: the arguments are bad")
+        sys.exit(1)
+    print(sos(morse_code))
+
+
+if __name__ == "__main__":
+    main()
+
+# Output
+
+"""
+$> python sos.py "sos" | cat -e
+... --- ...$
+$> python sos.py 'h$llo'
+AssertionError: the arguments are bad
+$>
+"""
