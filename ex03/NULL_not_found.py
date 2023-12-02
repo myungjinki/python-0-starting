ad72219b9af818d04ae8c973c6819b71878e7d36
42-mki <mki@student.42seoul.kr>
2023-12-02 12:04:44 +0900

    Add ex03

---
+++ ex03/NULL_not_found.py
@@ -0,0 +1,17 @@
+import math
+
+
+def NULL_not_found(object: any) -> int:
+    if isinstance(object, type(None)) and object is None:
+        print(f"Nothing: {object} {object.__class__}")
+    elif isinstance(object, bool) and object == False:
+        print(f"Fake: {object} {object.__class__}")
+    elif isinstance(object, int) and object == 0:
+        print(f"Zero: {object} {object.__class__}")
+    elif isinstance(object, float) and math.isnan(object):
+        print(f"Cheese: {object} {object.__class__}")
+    elif isinstance(object, str) and object == "":
+        print(f"Empty: {object.__class__}")
+    else:
+        print("Type not Found")
+    return 1
